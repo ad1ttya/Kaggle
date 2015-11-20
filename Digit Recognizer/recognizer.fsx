2c8594847b6974854150c79d3b6b9511b550a0e6
ad1ttya <ad1ttya@hotmail.com>
2015-11-21 02:14:02 +0530

    added euclidean dist model

--- Digit Recognizer/recognizer.fsx
+++ Digit Recognizer/recognizer.fsx
@@ -2,6 +2,8 @@ open System.IO
 
 type Observation = { Label : string; Pixels: int [] }
 
+type Distance = int[] * int[] -> int 
+
 let toObserve(csvData: string) =
   let columns = csvData.Split(',')
   let label = columns.[0]
@@ -21,18 +23,24 @@ let manhattanDistance (pixels1,pixels2) =
   |> Array.map (fun (x,y) -> abs (x-y))
   |> Array.sum
   
-let train (trainingset:Observation[]) =
+let euclideanDistance (pixels1, pixels2) =
+  Array.zip pixels1 pixels2
+  |> Array.map (fun (x,y) -> pown (x-y) 2)
+  |> Array.sum
+  
+let train (trainingset:Observation[]) (dist: Distance) =
   let classify (pixels:int[]) =
     trainingset
-    |> Array.minBy (fun x -> manhattanDistance (x.Pixels, pixels))
+    |> Array.minBy (fun x -> dist (x.Pixels, pixels))
     |> fun x -> x.Label
   classify
   
-let classifier = train trainingData
+let classifierManhattan = train trainingData manhattanDistance
+let classifierEuclidean = train trainingData euclideanDistance
 
 let validationPath = @"C:\Workspace\Kaggle\Digit Recognizer\data\validationsample.csv"
 let validationData = reader validationPath
 
 validationData
-  |> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+  |> Array.averageBy (fun x -> if classifierEuclidean x.Pixels = x.Label then 1. else 0.)
   |> printfn "Correct: %.3f"
